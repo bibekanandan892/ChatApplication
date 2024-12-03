33ca1fc6ae6765e2f4345e45594b2b9d45ccb531
BIBEKANANDA NAYAK <48781831+bibekanandan892@users.noreply.github.com>
2024-12-03 14:18:41 +0530

    Added search screen

---
+++ app/src/main/java/com/bibek/chatapplication/presentation/screen/search/SearchScreen.kt
@@ -0,0 +1,10 @@
+package com.bibek.chatapplication.presentation.screen.search
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchScreen(
+    uiState: SearchState, onEvent: (SearchEvent) -> Unit = {}
+) {
+
+}
