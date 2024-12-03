5c15a6baf13c1d1b136de381fa9a0c4834ae03a4
BIBEKANANDA NAYAK <48781831+bibekanandan892@users.noreply.github.com>
2024-12-03 14:16:53 +0530

    Added signup state

---
+++ app/src/main/java/com/bibek/chatapplication/presentation/screen/signup/SignupState.kt
@@ -0,0 +1,13 @@
+package com.bibek.chatapplication.presentation.screen.signup
+
+data class SignupState(
+    val isLoading: Boolean = false,
+    val gender : Gender? = null,
+    val name : String = ""
+    )
+
+sealed class Gender(val name : String){
+    data object Male: Gender(name ="ذكر")
+    data object Female : Gender(name =  "أنثى")
+    data object Both : Gender(name = "الكل")
+}
